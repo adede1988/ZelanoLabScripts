6b00306b636b1d63a8edf7dffaf2d5e0113eab0d
Adam Dede <30808263+adede1988@users.noreply.github.com>
2026-02-28 07:26:30 -0600

    Update singleChanEEGSubmit.sh

--- BashScripts/singleChanEEGSubmit.sh
+++ BashScripts/singleChanEEGSubmit.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1 ## how many computers do you need
 #SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
 #SBATCH --time=4:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=8G ## how much RAM do you need per CPU (this affects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --mem-per-cpu=4G ## how much RAM do you need per CPU (this affects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name="sc\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
 #SBATCH --output=sc.%a.out ## use the jobid (A) and the specific job index (a) to name your log file
 #SBATCH --error=sc.%a.err 
