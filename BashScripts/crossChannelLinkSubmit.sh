e908312c1e51d8a1db304121129fcddae04d429d
Adam Dede <30808263+adede1988@users.noreply.github.com>
2026-03-05 10:52:08 -0600

    Update crossChannelLinkSubmit.sh

--- BashScripts/crossChannelLinkSubmit.sh
+++ BashScripts/crossChannelLinkSubmit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=p33197  ## YOUR ACCOUNT pXXXX or bXXXX
 #SBATCH --partition=short  ### PARTITION (buyin, short, normal, w10001, etc)
-#SBATCH --array=1-609 ## number of jobs to run "in parallel" 
+#SBATCH --array=226-257 ## number of jobs to run "in parallel" 
 #SBATCH --nodes=1 ## how many computers do you need
 #SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
 #SBATCH --time=0:30:00 ## how long does this need to run (remember different partitions have restrictions on this param)
